5f068ad29ca9d5bebb8f4e342298015906aa079d
diogo <diogoalexandredecodasilvapt@gmail.com>
2025-12-02 11:12:13 +0000

    Enhance tokenizer with improved error handling and support for additional token types

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include "../internal/core.h"
 
+void* operator new(const size_t s){
+    std::cerr << "Allocating memory for " << s << " bytes: " << s << '\n';
+    return malloc(s);
+}
+
 int main() {
     const auto stream = nova::file::readSource();
     const auto tokens = Tokenizer::tokenize(stream);
